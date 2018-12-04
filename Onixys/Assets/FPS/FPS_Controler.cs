c009e8fd43b573147f8d5578bda2a2efc06294b3
DorianCanville <45595434+DorianCanville@users.noreply.github.com>
2018-12-04 21:35:57 +0100

    Initial commit

---
+++ Onixys/Assets/FPS/FPS_Controler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Controler : MonoBehaviour {
+    public float speed = 10f;
+    public float sensitivity = 2f;
+
+    CharacterController player;
+    public GameObject eyes;
+    float FB;
+    float LR;
+
+    float rotX;
+    float rotY;
+
+    bool Sprint = false;
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (Sprint == true)
+            {
+                Sprint = false;
+                speed = 10f;
+            }
+            else Sprint = true;
+        }
+        if (Sprint == true) speed = 20f; 
+
+        FB = Input.GetAxis("Horizontal") * speed;
+        LR = Input.GetAxis("Vertical") * speed;
+
+         rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        Vector3 Mouvement = new Vector3(FB, 0, LR);
+        transform.Rotate(0, rotX, 0);
+        eyes.transform.Rotate(-rotY, 0, 0);
+        Mouvement = transform.rotation * Mouvement;
+        player.Move (Mouvement * Time.deltaTime);
+	}
+}
---
+++ Onixys/Assets/Programme/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("il y a deja un buildmanager dans la scene");
+        }
+        instance = this;
+    }
+    #endregion
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
